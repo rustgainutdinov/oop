3b6987b57f5ffa8513b6f57f0820f473056c0ce4
rustgainutdinov <46785191+rustgainutdinov@users.noreply.github.com>
2019-02-23 08:51:13 +0300

    lw1

--- lw1/radix/index.js
+++ lw1/radix/index.js
@@ -1,67 +1,77 @@
 const alphabet = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
 function errHandler(err) {
-  console.log(err.message);
-  process.exit();
+	console.log(err.message);
+	process.exit();
 }
 
-function getSymbolByNumber(num, cb) {
-  const symbol = alphabet[num];
-  if (!symbol) cb(new Error('The number to be transferred cannot be more than 35 and less than 0'));
-  return symbol
+function getSymbolByNumber(num) {
+	const symbol = alphabet[num];
+	if (!symbol) throw new Error('The number to be transferred cannot be more than 35 and less than 0');
+	return symbol
 }
 
-function getNumberBySymbol(symbol, cb) {
-  const num = alphabet.indexOf(symbol);
-  if (num === -1) cb(new Error('Letter is not found'));
-  return Number(num)
+function getNumberBySymbol(symbol,) {
+	const num = alphabet.indexOf(symbol);
+	if (num === -1) throw new Error('Letter is not found');
+	return Number(num)
 }
 
-function StringToInt(str, radix, cb) {
-  const symbolsArr = str.split('');
-  var i = 0;
-  const result = symbolsArr.reduceRight((sum, symbol) => {
-    const num = getNumberBySymbol(symbol, err => {
-      if (err) cb(err)
-    });
-    if (!Number.isSafeInteger((num * Math.pow(radix, i)) + sum)) {
-      cb(new Error('This number is not safe'));
-    }
-    return (num * Math.pow(radix, i++)) + sum;
-  }, 0);
-  return result
+function stringToInt(str, radix) {
+	const isNegativeNum = str.indexOf('-') === -1 ? false : true;
+	str = isNegativeNum ? str.replace('-', '') : str;
+	const symbolsArr = str.split('');
+	var i = 1;
+	const result = symbolsArr.reduceRight((sum, symbol) => {
+		const num = getNumberBySymbol(symbol, err => {
+			if (err) throw err
+		});
+		var tempResult = num * i + sum;
+		if (!Number.isSafeInteger(tempResult)) {
+			throw new Error('This number is not safe');
+		}
+		i = i * radix;
+		return tempResult;
+	}, 0);
+	return Number(isNegativeNum ? '-' + result : result)
 }
 
-function IntToString(num, radix, cb) {
-  var str = '';
-  while (num >= radix) {
-    str += getSymbolByNumber(Math.floor(num % radix), err => {
-      if (err) cb(err)
-    });
-    num = Math.floor(num / radix);
-  }
-  str += getSymbolByNumber(num, err => {
-    if (err) cb(err)
-  });
-  return str.split('').reverse().join('')
+function intToString(num, radix) {
+	const isNegativeNum = num < 0 ? true : false;
+	num = isNegativeNum ? num * -1 : num;
+	var str = '';
+	while (num >= radix) {
+		str += getSymbolByNumber(num % radix, err => {
+			if (err) throw err
+		});
+		num = Math.floor(num / radix);
+	}
+	str += getSymbolByNumber(num, err => {
+		if (err) throw err
+	});
+	return (isNegativeNum ? '-' : '') + str.split('').reverse().join('')
 }
 
-if (process.argv.length !== 5) {  
-  const errMsg = 'Invalid argument count\nUsage: ./radix <source notation> <destination notation> <value>';
-  errHandler(new Error(errMsg));
+if (process.argv.length !== 5) {
+	const errMsg = 'Invalid argument count\nUsage: ./radix <source notation> <destination notation> <value>';
+	errHandler(new Error(errMsg));
 }
 
-const isNegativeNum = process.argv[4].indexOf('_') === -1 ? false : true;
 const sourceRadix = Number(process.argv[2]);
 const destinationRadix = Number(process.argv[3]);
-const value = isNegativeNum ? process.argv[4].substring(1) : process.argv[4];
+const value = process.argv[4];
+let resultValue;
 
-const intermediateValue = StringToInt(value, sourceRadix, err => {
-  if (err) errHandler(err)
-});
+try {
+	const intermediateValue = stringToInt(value, sourceRadix, err => {
+		if (err) errHandler(err)
+	});
 
-const resultValue = (isNegativeNum ? '-' : '') + IntToString(intermediateValue, destinationRadix, err => {
-  if (err) errHandler(err)
-});
+	resultValue = intToString(intermediateValue, destinationRadix, err => {
+		if (err) errHandler(err)
+	});
+} catch (e) {
+	errHandler(e);
+}
 
 console.log(resultValue);
--- lw1/radix/test.sh
+++ lw1/radix/test.sh
@@ -1,6 +1,6 @@
 npm run build
 
-./radix $(cat tests/testIn1.txt) > tests/testOut1.txt
+./radix 16 10 > tests/testOut1.txt
 if cmp tests/testOut1.txt tests/testResult1.txt ;
 then
     echo "Success arguments num"
@@ -8,7 +8,7 @@ else
     echo "Error arguments num"; exit 1
 fi
 
-./radix $(cat tests/testIn2.txt) > tests/testOut2.txt
+./radix 16 10 1F > tests/testOut2.txt
 if cmp tests/testOut2.txt tests/testResult2.txt ;
 then
     echo "Success test"
@@ -16,7 +16,7 @@ else
     echo "Error test"; exit 1
 fi
 
-./radix $(cat tests/testIn3.txt) > tests/testOut3.txt
+./radix 3 8 21221 > tests/testOut3.txt
 if cmp tests/testOut3.txt tests/testResult3.txt ;
 then
     echo "Success test"
@@ -24,7 +24,7 @@ else
     echo "Error test"; exit 1
 fi
 
-./radix $(cat tests/testIn4.txt) > tests/testOut4.txt
+./radix 10 16 9007199254740991 > tests/testOut4.txt
 if cmp tests/testOut4.txt tests/testResult4.txt ;
 then
     echo "Success max val test"
@@ -32,7 +32,7 @@ else
     echo "Error max val test"; exit 1
 fi
 
-./radix $(cat tests/testIn5.txt) > tests/testOut5.txt
+./radix 10 6 0 > tests/testOut5.txt
 if cmp tests/testOut5.txt tests/testResult5.txt ;
 then
     echo "Success zero test"
@@ -40,7 +40,7 @@ else
     echo "Error zero test"; exit 1
 fi
 
-./radix $(cat tests/testIn6.txt) > tests/testOut6.txt
+./radix 10 16 9007199254740992 > tests/testOut6.txt
 if cmp tests/testOut6.txt tests/testResult6.txt ;
 then
     echo "Success max val test"
@@ -48,7 +48,7 @@ else
     echo "Error max val test"; exit 1
 fi
 
-./radix $(cat tests/testIn7.txt) > tests/testOut7.txt
+./radix 10 8 -8377 > tests/testOut7.txt
 if cmp tests/testOut7.txt tests/testResult7.txt ;
 then
     echo "Success negative number test"
